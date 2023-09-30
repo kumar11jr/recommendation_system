3898bb5d2f3a1c452511b96e1b85ae3500fd6858
kumar11jr <kumar11jr@gmail.com>
2023-10-01 00:44:27 +0530

    getting recommendation

--- Recommendation_backend/Recommendation_backend/views.py
+++ Recommendation_backend/Recommendation_backend/views.py
@@ -3,17 +3,37 @@ from django.views.decorators.csrf import csrf_exempt
 import pickle
 import pandas as pd
 
+
+def recommend(movie):
+    movie_index = movies_df[movies_df["title"] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:10]
+
+    recommended_movies = []
+    for i in movies_list:
+        recommended_movies.append(movies_df.iloc[i[0]].title)
+    return recommended_movies
+
+
+
 @csrf_exempt
 def ml(request):
     data = request.POST.get('data')
-    print(data)
-    return JsonResponse({'result':'result'})
+    recommendation = recommend(data)
+    # print(recommendation)
+    # print("hello")
+    return JsonResponse(recommendation,safe=False)
+
+
+# loading models && dataframes
+movies_df = pd.read_pickle("movies.pkl")
+movies_data = movies_df.to_dict(orient="records")
+similarity = pickle.load(open('similarity.pkl',"rb"))
 
 
 def load_movies(request):
     try:
-        movies_df = pd.read_pickle("movies.pkl")
-        movies_data = movies_df.to_dict(orient="records")
+        # print(movies_df)
         return JsonResponse(movies_data, safe=False)
     except FileNotFoundError:
         return JsonResponse({"error": "File not found"}, status=404)
--- front_end-recommnedation/src/App.jsx
+++ front_end-recommnedation/src/App.jsx
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [recommend, setRecommend] = useState([]);
+
   useEffect(() => {
     axios
       .get("http://127.0.0.1:8000/movies/")
       .then((response) => {
         const movieData = response.data;
+        console.log(movieData);
         const movieTitles = movieData.map((movie) => movie.title);
         setMovies(movieTitles);
       })
@@ -15,57 +21,73 @@ function App() {
       });
   }, []);
 
-  const [formData, setFormData] = useState("");
   const formD = new FormData();
-  formD.append("data", formData);
+  formD.append("data", searchResults);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
       .post("http://127.0.0.1:8000/", formD)
       .then((resp) => {
-        console.log(resp);
+        setRecommend(resp.data); // Store the recommended movies in state
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
-  const [searchTerm, setSearchTerm] = useState("");
-  const [searchResults, setSearchResults] = useState([]);
   const handleSearch = (e) => {
     const query = e.target.value.toLowerCase();
     setSearchTerm(query);
 
-    // Filter movies that contain the search term
-    const results = movies.filter((movie) =>
-      movie.toLowerCase().includes(query)
-    );
-    setSearchResults(results);
+    // Show suggestions if the input is not empty
+    if (query) {
+      const results = movies.filter((movie) =>
+        movie.toLowerCase().includes(query)
+      );
+      setSearchResults(results);
+    } else {
+      // Clear suggestions when the input is empty
+      setSearchResults([]);
+    }
   };
 
   return (
     <>
       <div>
         <h1 className="">Movie Search</h1>
-        <div class="search-container">
-          <input
-            type="text"
-            value={searchTerm}
-            onChange={handleSearch}
-            class="search-input"
-            placeholder="Search for a movie"
-          />
-          <button class="search-button">Search</button>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="search-container">
+            <input
+              type="text"
+              value={searchTerm}
+              onChange={handleSearch}
+              className="search-input" // Use className instead of class
+              placeholder="Search for a movie"
+            />
+            <button type="submit" className="search-button">
+              Search
+            </button>{" "}
+            {/* Use className */}
+          </div>
+        </form>
         <ul>
-          {searchResults.length > 0 ? (
-            searchResults
-              .slice(0, 10)
-              .map((title, index) => <p key={index}>{title}</p>)
-          ) : (
-            <p></p>
-          )}
+          {searchResults.length > 0
+            ? searchResults
+                .slice(0, 10)
+                .map((title, index) => <p key={index}>{title}</p>)
+            : null}
+
+          {searchResults.length !== 0 && recommend.length > 0 ? (
+            <div>
+              <h2>Recommended Movies</h2>
+              <ul>
+                {recommend.map((movie, index) => (
+                  <li key={index}>{movie}</li>
+                ))}
+              </ul>
+            </div>
+          ) : null}
         </ul>
       </div>
     </>
